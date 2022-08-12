fc9cc18ad1f8cbb53a8a1dbfe9d3807003dac17b
Colin Kelley <colindkelley@gmail.com>
2022-08-11 17:22:41 -0700

    issue-#5: check VERSION pattern to be major.minor.patch, each one or more digits

--- spec/async/http/cache_spec.rb
+++ spec/async/http/cache_spec.rb
@@ -1,6 +1,6 @@
 
 RSpec.describe Async::HTTP::Cache do
 	it "has a version number" do
-		expect(Async::HTTP::Cache::VERSION).not_to be nil
+		expect(Async::HTTP::Cache::VERSION).to match(/\A\d+\.\d+\.\d+\z/)
 	end
 end
